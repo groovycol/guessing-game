ee08d30214df80f0888aeea1c88ffa583c9d60f1
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 12:38:04 -0700

    Greeting the Player

---
+++ game.py
@@ -0,0 +1,5 @@
+# Put your code here
+import random
+name = raw_input ("Howdy, What's your name? ")
+print "%s, I'm thinking of a number between 1 and 100." % (name)
+print "Try to guess my number."
