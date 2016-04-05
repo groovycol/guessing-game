f82489f8c6c6395752af0c63b21ed60150b639c3
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 14:43:51 -0700

    Full Guessing Game

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 import random
 
 number = None
-guess = int()
+guess =  None
 name = raw_input ("Howdy, What's your name? ")
 print "%s, I'm thinking of a number between 1 and 100." % (name)
 print "Try to guess my number."
@@ -10,11 +10,12 @@ number = random.randint(1,100)
 
 while guess != number:
     print number
-    guess = raw_input("Your guess? ")
+    type (number)
+    guess = int(raw_input("Your guess? "))
     if guess > number:
         print "Your guess is too high, try again!"
     elif guess < number:
         print "Your number is too low, try again!"
     else: 
-        print "Huzzah! you picket the right number, %s." % (number)        
+        print "Huzzah! you picked the it! The number is %s." % (number)        
 
