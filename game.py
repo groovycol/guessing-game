e2ebe76fbad900a1ecbebacb2582e866488794af
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 12:43:22 -0700

    Getting the Player's Number

--- game.py
+++ game.py
@@ -1,5 +1,11 @@
 # Put your code here
 import random
+
+number=None
+
 name = raw_input ("Howdy, What's your name? ")
 print "%s, I'm thinking of a number between 1 and 100." % (name)
 print "Try to guess my number."
+number = random.randint(1,100)
+guess = raw_input("Your guess? ")
+
