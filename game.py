dbe3ed6bad6b8dc95828b26623022a03a426db02
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 14:48:32 -0700

    Test complete version. Ready for code review

--- game.py
+++ game.py
@@ -1,16 +1,12 @@
 # Put your code here
 import random
-
 number = None
 guess =  None
 name = raw_input ("Howdy, What's your name? ")
 print "%s, I'm thinking of a number between 1 and 100." % (name)
 print "Try to guess my number."
 number = random.randint(1,100)
-
 while guess != number:
-    print number
-    type (number)
     guess = int(raw_input("Your guess? "))
     if guess > number:
         print "Your guess is too high, try again!"
